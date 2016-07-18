e596fe3af8049d37d4c4e3f976ede753f7e55d5c
Gary Qian <gary@qian.ninja>
2016-07-18 01:49:28 -0400

    iOS setup

--- Assets/GooglePlayGames/GameInfo.cs
+++ Assets/GooglePlayGames/GameInfo.cs
@@ -38,8 +38,8 @@ namespace GooglePlayGames {
         private const string UnescapedRequireGooglePlus = "REQUIRE_GOOGLE_PLUS";
 
         public const string ApplicationId = "361806534742"; // Filled in automatically
-        public const string IosClientId = ""; // Filled in automatically
-        public const string WebClientId = ""; // Filled in automatically
+        public const string IosClientId = "361806534742-119ucmq8ie8ucffb8ea54pucktbp8aj5.apps.googleusercontent.com"; // Filled in automatically
+        public const string WebClientId = "361806534742-119ucmq8ie8ucffb8ea54pucktbp8aj5.apps.googleusercontent.com"; // Filled in automatically
         public const string NearbyConnectionServiceId = "";
 
         public static bool RequireGooglePlus()
